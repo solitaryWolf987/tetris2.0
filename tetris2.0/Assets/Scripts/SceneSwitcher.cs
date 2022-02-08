7625564675fbe0f692c0001e4c12b6036fbfe738
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-02-08 12:58:15 +0000

    added tmp addon, scene can be switched, created main and options menus

--- tetris2.0/Assets/Scripts/SceneSwitcher.cs
+++ tetris2.0/Assets/Scripts/SceneSwitcher.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float autoLoadNextLevelAfter;
 
-    // Update is called once per frame
-    void Update()
+    
+    /**
+     void Start() {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Level auto load disabled, use a positive value.");
+        }
+        else {
+            invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+    */
+
+
+    public void LoadNextLevel()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    //specific level to load; named on the button UI
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    //quit application
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        //SceneManager.Quit ();
     }
 }
