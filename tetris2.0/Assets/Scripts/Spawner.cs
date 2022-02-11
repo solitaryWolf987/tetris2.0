659bb704d89f65eb53a36d63219a03f038bcf346
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-02-11 16:23:34 +0000

    created spawner code

---
+++ tetris2.0/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] groups;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Spawns a Block shape out of the prefabs
+    public void spawnNext()
+    {
+        int i = Random.Range(0, groups.Length);
+
+        Instantiate(groups[1], transform.position, Quaternion.identity);
+    }
+}
