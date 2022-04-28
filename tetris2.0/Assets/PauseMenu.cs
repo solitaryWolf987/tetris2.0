4fb66d36a30b3f482be5ebf9e5fd695c9c07f038
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-04-28 14:41:20 +0100

    start of pause menu

---
+++ tetris2.0/Assets/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIfPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
