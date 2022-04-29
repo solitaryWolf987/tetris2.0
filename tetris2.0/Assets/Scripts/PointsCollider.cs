36ac0a650f522180cde595b1964e16b2e2325f9e
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-04-29 16:04:46 +0100

    when collides, adds 50 points

---
+++ tetris2.0/Assets/Scripts/PointsCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsCollider : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        Debug.Log("Star collide");
+        ScoringSystem.instance.IncreaseScore(50);
+    }
+}
