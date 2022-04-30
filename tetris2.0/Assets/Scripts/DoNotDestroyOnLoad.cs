24b4b3e8c13c5c544b2093ef08c811bf9e7e59c1
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-04-30 20:43:45 +0100

    music playing start

---
+++ tetris2.0/Assets/Scripts/DoNotDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroyOnLoad : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] musicObject = GameObject.FindGameObjectsWithTag("music");
+        if (musicObject.Length > 1)
+        {
+            Debug.Log("Destroy on load: ");
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        //audioSource = GetComponent<AudioSource>();
+        Debug.Log("Don't Destroy on load: ");
+    }
+}
---
+++ tetris2.0/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    //public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+    private float musicVolume = 0.8f;
+    public Slider volumeSlider = null;
+    public GameObject MusicObject;
+    
+
+
+    private void Start()
+    {
+        MusicObject = GameObject.FindWithTag("music");
+        audioSource = MusicObject.GetComponent<AudioSource>();
+
+        //musicVolume = PlayerPrefs.GetFloat("volume");
+        audioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
+
+    }
+   
+
+    void Update()
+    {
+        audioSource.volume = musicVolume;
+        //PlayerPrefs.SetFloat("volume", musicVolume);
+    }
+   
+    public void SetVolume (float volume)
+    {
+        musicVolume = volume;
+    }
+
+
+
+}
